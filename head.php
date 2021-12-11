7052f2b82b322437c63e9252c190e566dbff7c95
Fartrivi8 <88885049+Fartrivi8@users.noreply.github.com>
2021-12-11 13:22:26 +0700

    inisiasi script web
    upload script ke repo

---
+++ head.php
@@ -0,0 +1,16 @@
+<meta charset="utf-8">
+<title><?= $title ?></title>
+<link rel="icon" href=<?= $icon ?>>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="iLand Multipurpose Landing Page Template">
+<meta name="keywords" content="iLand HTML Template, iLand Landing Page, Landing Page Template">
+<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all" />
+<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900" rel="stylesheet">
+<link rel="stylesheet" href="css/animate.css">
+<!-- Resource style -->
+<link rel="stylesheet" href="css/cssedit.css?version=3">
+<link rel="stylesheet" href="css/owl.carousel.css">
+<link rel="stylesheet" href="css/owl.theme.css">
+<link rel="stylesheet" href="css/ionicons.min.css">
+<!-- Resource style -->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
---
+++ index.php
@@ -0,0 +1,28 @@
+<!-- Data Variabel -->
+<?php
+    /* Judul Website */
+    $title = "PS ITB Official Website";
+    /* icon */
+    $icon = "images/logo-ps.ico";
+    /* Section 1 */
+    $judul_s1 = "Kegiatan Unit PS ITB"
+?>
+
+<!-- HTML -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include 'head.php' ?>
+</head>
+<body>
+    <div class="wrapper">
+        <!-- Navbar -->
+        <?php include "navbar.php" ?>
+        <!-- Main Section -->
+        <?php include "main-section.php" ?>
+    </div>
+    
+    <!-- JS Plugin -->
+    <?php include "js-plugin.php"?>
+</body>
+</html>
